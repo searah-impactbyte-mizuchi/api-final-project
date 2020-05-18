ecf9ecbb7df651f358cb7b703ab447f09c04b6cf
Miftah Faris <miftah.faris07@yahoo.co.id>
2020-05-18 10:29:43 +0700

    fix route trip getbyid

--- routes/trips/controller.js
+++ routes/trips/controller.js
@@ -1,63 +1,67 @@
-const { User, Trip } = require("../../models")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const { User, Trip } = require("../../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 module.exports = {
     getAll: async (req, res) => {
         try {
-            const result = await Trip.findAll({})
+            const result = await Trip.findAll({});
 
             res.status(200).json({
                 message: "Get all trip data for users",
                 data: result,
-            })
+            });
         } catch (error) {
             console.log(error);
-
         }
     },
     getBYID: async (req, res) => {
-        const { id } = req.params
+        const { id } = req.params;
         try {
             const result = await Trip.findAll({
                 where: {
-                    id: id
-                }, 
-                raw: true
-            })
+                    id: id,
+                },
+                raw: true,
+            });
 
-            const members = result[0].members.split(",")
-            // console.log(members);
-            const memberResult = await members.map(async item  => {
-                const result = await User.findAll({
-                    where: {
-                        id : item,
-                    },
-                    raw : true
-                })
-                console.log(result);
-                
-                return result
-            })
+            const membersID = result[0].members.split(",");
+            const members = await User.findAll({
+                raw: true,
+            });
 
-            console.log(memberResult);
-            
+            const array = [];
+            members.map((item) => {
+                membersID.map((id) => {
+                    if (item.id == id) {
+                        array.push(item);
+                    }
+                });
+            });
 
-            
+            result[0].members = array;
+
             res.status(200).json({
                 message: "Get trip data by ID",
                 data: result,
-            })
-            console.log(result);
-            
+            });
         } catch (error) {
             console.log(error);
-
         }
     },
     create: async (req, res) => {
         try {
-            const { location, destination, from, to, title, description, meetupPoint, user_id, members } = req.body
+            const {
+                location,
+                destination,
+                from,
+                to,
+                title,
+                description,
+                meetupPoint,
+                user_id,
+                members,
+            } = req.body;
             const result = await Trip.create({
                 location,
                 destination,
@@ -67,26 +71,24 @@ module.exports = {
                 description,
                 meetupPoint,
                 user_id,
-                members
-            })
+                members,
+            });
 
             res.status(200).json({
                 message: "Create new data successfully",
                 data: result,
-            })
+            });
             res.status(401).json({
                 message: "Email must be unique",
                 data: result,
-            })
+            });
         } catch (error) {
             console.log(error);
         }
     },
     update: async (req, res) => {
         try {
-            const { location, destination, from, to, title, description, meetupPoint } = req.body
-            const { id } = req.params
-            const result = await Trip.update({
+            const {
                 location,
                 destination,
                 from,
@@ -94,37 +96,49 @@ module.exports = {
                 title,
                 description,
                 meetupPoint,
-            },
+            } = req.body;
+            const { id } = req.params;
+            const result = await Trip.update(
                 {
-                    where: { 
-                        id: id 
-                    }
-                })
-            const getAll = await Trip.findAll({})
+                    location,
+                    destination,
+                    from,
+                    to,
+                    title,
+                    description,
+                    meetupPoint,
+                },
+                {
+                    where: {
+                        id: id,
+                    },
+                }
+            );
+            const getAll = await Trip.findAll({});
 
             res.status(200).json({
                 message: "Update new data successfully",
                 data: getAll,
-            })
+            });
         } catch (error) {
             console.log(error);
-
         }
     },
     deleteByID: async (req, res) => {
         try {
-            const { id } = req.params
+            const { id } = req.params;
             // console.log(id);
 
             const result = await Trip.destroy({
                 where: {
-                    id: id
-                }
-            })
+                    id: id,
+                },
+            });
 
             res.status(200).json({
-                message: `Movie with id : ${id} is successfully deleted`, data: result
-            })
+                message: `Movie with id : ${id} is successfully deleted`,
+                data: result,
+            });
         } catch (error) {
             console.log(error);
         }
@@ -159,4 +173,4 @@ module.exports = {
     //         console.log(error);
     //     }
     // }
-}
+};
